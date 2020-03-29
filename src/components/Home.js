f25d1b01397c24a8b1c7f09dd39f4b5bc8794dec
Andy Durette <duretteandy@gmail.com>
2020-03-28 20:27:31 -0400

    Backgournd image fix for homepage to deal with wepack css issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,78 +1,44 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive'
+import andyDuretteC from '../img/andyDuretteC.jpg'; // Tell webpack this JS file uses this image
+import andyDuretteMobileC from '../img/andyDuretteMobileC.jpg'; // Tell webpack this JS file uses this image
 
 const Home = () => {
-  /*
-  var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-  
 
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
-    document.body.appendChild(css);
-  };
-  */
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 901px)'
+  })
 
+  console.log(isDesktopOrLaptop)
   return(
-    <section id="home">
+
+
+
+    <React.Fragment>
+    {(isDesktopOrLaptop) ? 
+      <section id="home" style={{backgroundImage: `url(${andyDuretteC})`}}>
       <div className="center-container">
         <div className="intro-container">
           <div className="intro-text">
             <div className="intro-heading">I'm Andy</div>
             <div className="intro-lead-in">A React &amp; Node JS Full Stack Developer</div>
-            {/* <span className="txt-rotate" data-period="2000" data-rotate='[ "Full Stack Developer", "Web & Graphic Designer", "Amateur Cook & Baker", "Life Long Learner" ]'></span> */}
             </div>
         </div>
       </div>
     </section>
+    :  
+    <section id="home" style={{backgroundImage: `url(${andyDuretteMobileC})`}}>
+    <div className="center-container">
+      <div className="intro-container">
+        <div className="intro-text">
+          <div className="intro-heading">I'm Andy</div>
+          <div className="intro-lead-in">A React &amp; Node JS Full Stack Developer</div>
+          </div>
+      </div>
+    </div>
+  </section>}
+   
+    </React.Fragment>
   );
 }
 
