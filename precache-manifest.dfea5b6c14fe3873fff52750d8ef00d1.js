cd92fc5ce24aeb95e1d9cbed5ae4bf22a79a5426
Andy Durette <duretteandy@gmail.com>
2021-01-31 23:05:44 -0500

    Updated image paths

---
+++ precache-manifest.dfea5b6c14fe3873fff52750d8ef00d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3816569acfd9ef802abfa2612a73488c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "10a5c5d23a52d57f28d0",
+    "url": "/react-portfolio/static/css/main.79bea85e.chunk.css"
+  },
+  {
+    "revision": "0d4ab9a424b99c770150",
+    "url": "/react-portfolio/static/js/2.de4a66d5.chunk.js"
+  },
+  {
+    "revision": "60ecaad85320638366e6b61932d38edb",
+    "url": "/react-portfolio/static/js/2.de4a66d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a5c5d23a52d57f28d0",
+    "url": "/react-portfolio/static/js/main.f75d500c.chunk.js"
+  },
+  {
+    "revision": "9f289a7f9c113993272b",
+    "url": "/react-portfolio/static/js/runtime-main.f68edf73.js"
+  },
+  {
+    "revision": "df41ba3c69602b2577c79c86761498a0",
+    "url": "/react-portfolio/static/media/andyDuretteC.df41ba3c.jpg"
+  },
+  {
+    "revision": "62eae05e39750a0839d6aaeef808a898",
+    "url": "/react-portfolio/static/media/andyDuretteMobileC.62eae05e.jpg"
+  }
+]);
