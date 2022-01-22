404642f8abd58c28e951d03c7a2d46367ad77cca
Andy Durette <duretteandy@gmail.com>
2022-01-22 12:41:22 -0500

    Removed outdated contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,10 +15,6 @@ const Contact = () => {
           <div id="contact-info">
             <p>Feel free to look through my professional profiles and social media links below. Please contact me if you enjoy my work and feel I would be an asset to your team or an ideal candidate to work on your next project.</p>
             <div className="contact-item">
-              <img src="content/img/logos/phone.svg" alt="phone" width="25px" height="25px" />
-              <p>647-607-7597</p>
-            </div>
-            <div className="contact-item">
               <img src="content/img/logos/email.svg" alt="email" width="25px" height="25px"/>
               <p>andy@andydurette.com</p>
             </div>
